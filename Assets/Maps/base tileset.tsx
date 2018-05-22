fe1087de6318daa15e99cec513c620957a28c00a
Alon Jacobson <alojaco1@gmail.com>
2018-05-22 12:46:56 -0400

    Added tile map

---
+++ Assets/Maps/base tileset.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="base tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="../Downloads/roguelike-pack/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="Dirt" tile="578"/>
+ </terraintypes>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="10" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="10" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="10" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index">
+   <object id="10" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup draworder="index">
+   <object id="10" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup draworder="index">
+   <object id="10" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <objectgroup draworder="index">
+   <object id="10" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="117">
+  <objectgroup draworder="index">
+   <object id="10" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="118">
+  <objectgroup draworder="index">
+   <object id="10" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="518" terrain="0,0,0,"/>
+ <tile id="519" terrain="0,0,,0"/>
+ <tile id="520" terrain=",,,0"/>
+ <tile id="521" terrain=",,0,0"/>
+ <tile id="522" terrain=",,0,"/>
+ <tile id="526">
+  <objectgroup draworder="index">
+   <object id="11" x="4" y="1">
+    <polygon points="0,0 8,0 8,1 10,1 10,2 11,2 11,4 12,4 12,10 11,10 11,11 9,11 9,12 6,12 6,13 7,13 7,15 1,15 1,13 2,13 2,12 -1,12 -1,11 -3,11 -3,10 -4,10 -4,4 -3,4 -3,2 -2,2 -2,1 0,1"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="575" terrain="0,,0,0"/>
+ <tile id="576" terrain=",0,0,0"/>
+ <tile id="577" terrain=",0,,0"/>
+ <tile id="578" terrain="0,0,0,0"/>
+ <tile id="579" terrain="0,,0,"/>
+ <tile id="634" terrain=",0,,"/>
+ <tile id="635" terrain="0,0,,"/>
+ <tile id="636" terrain="0,,,"/>
+ <tile id="643">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="2,0 14,0 14,4 15,4 15,8 16,8 16,11 13,11 13,12 10,12 10,14 11,14 11,16 5,16 5,14 6,14 6,12 3,12 3,11 0,11 0,8 1,8 1,4 2,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
 	public float moveSpeed;
+	private Animator animator;
+	private bool isMoving;
+	private Vector2 lastMove;
 
 	void Start () {
-		
+		animator = GetComponent<Animator>();
 	}
 
 	void Update () {
+		float moveX = Input.GetAxisRaw("Horizontal");
+		float moveY = Input.GetAxisRaw("Vertical");
 		float unit = moveSpeed * Time.deltaTime;
-		transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal") * unit, Input.GetAxisRaw("Vertical") * unit));
+		transform.Translate(new Vector2(moveX * unit, moveY * unit));
+		isMoving = Mathf.Abs(moveX) + Mathf.Abs(moveY) > 0;
+		if (isMoving)
+			lastMove = new Vector2(moveX, moveY);
+
+		animator.SetFloat("moveX", moveX);
+		animator.SetFloat("moveY", moveY);
+
+		animator.SetBool("isMoving", isMoving);
+
+		animator.SetFloat("lastMoveX", lastMove.x);
+		animator.SetFloat("lastMoveY", lastMove.y);
 	}
 }
