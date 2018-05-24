268b141dbac147c5859aa815367c50694788b794
Alon Jacobson <alojaco1@gmail.com>
2018-05-24 12:36:03 -0400

    Added collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 	public float moveSpeed;
 	private Animator animator;
+	private Rigidbody2D rigidbody;
 	private bool isMoving;
 	private Vector2 lastMove;
 
 	void Start () {
 		animator = GetComponent<Animator>();
+		rigidbody = GetComponent<Rigidbody2D>();
 	}
 
 	void Update () {
 		float moveX = Input.GetAxisRaw("Horizontal");
 		float moveY = Input.GetAxisRaw("Vertical");
-		float unit = moveSpeed * Time.deltaTime;
-		transform.Translate(new Vector2(moveX * unit, moveY * unit));
+		//float unit = moveSpeed * Time.deltaTime;
+		float unit = moveSpeed;
+
+		//transform.Translate(new Vector2(moveX * unit, moveY * unit));
+		rigidbody.velocity = new Vector2(moveX * unit, moveY * unit);
+
+
 		isMoving = Mathf.Abs(moveX) + Mathf.Abs(moveY) > 0;
 		if (isMoving)
 			lastMove = new Vector2(moveX, moveY);
